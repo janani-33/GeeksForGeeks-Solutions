c33d29768df0f787bc81b977f5f13c232fcdf6c6
Janani S <150319424+janani-33@users.noreply.github.com>
2024-07-26 10:33:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find position of set bit/find-position-of-set-bit.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.findPosition(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int findPosition(int N) {
+        if(Integer.bitCount(N)!=1)
+        return -1;
+        else{
+            String s=Integer.toBinaryString(N);
+            int n=s.length()-s.indexOf("1");
+            return n;
+        }
+    }
+};
