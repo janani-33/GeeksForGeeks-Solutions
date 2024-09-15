f091710a8b68026cb8a8d1a104e27314ddb11eea
Janani S <150319424+janani-33@users.noreply.github.com>
2024-09-15 12:27:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum Cost Path/minimum-cost-path.java
+++ Difficulty: Hard/Minimum Cost Path/minimum-cost-path.java
@@ -27,50 +27,39 @@ class GFG
 }
 // } Driver Code Ends
 
-
+class Tuple{
+    int r;
+    int c;
+    int d;
+    Tuple(int r,int c,int d){
+        this.r=r;
+        this.c=c;
+        this.d=d;
+    }
+}
 class Solution
 {
     //Function to return the minimum cost to react at bottom
 	//right cell from top left cell.
-	class Tuple
-    {
-        int row,col,cost;
-        Tuple(int row, int col,int cost){
-            this.row=row;
-            this.col=col;
-            this.cost=cost;
-        }
-    }
-    int [][] d={{1,0},{-1,0},{0,1},{0,-1}};
     public int minimumCostPath(int[][] grid)
     {
         int n=grid.length;
-        int [][] cost=new int[n][n];
-        for(int [] sub:cost) 
-        Arrays.fill(sub,1000000000);
-        PriorityQueue<Tuple> pq=new PriorityQueue<>((a,b)-> a.cost - b.cost);
+        int[][] cost=new int[n][n];
+        for(int i=0;i<n;i++)
+        Arrays.fill(cost[i],Integer.MAX_VALUE);
+        int x[]={-1,0,1,0};
+        int y[]={0,-1,0,1};
+        PriorityQueue<Tuple> pq=new PriorityQueue<>((a,b)->a.d-b.d);
         pq.add(new Tuple(0,0,grid[0][0]));
-        cost[0][0] = grid[0][0];        
-        while(!pq.isEmpty())
-        {
-            Tuple t = pq.remove();
-            int row = t.row;
-            int col = t.col;
-            int prev= t.cost;
-            
-            for(int [] dir:d)
-            {
-                int dx=row+dir[0];
-                int dy=col+dir[1];
-                
-                if(dx>=0 && dx<n  &&  dy>=0  &&  dy<n)
-                {
-                    int p=grid[dx][dy]+prev;
-                    if(cost[dx][dy]>p)
-                    {
-                        cost[dx][dy]=p;
-                        pq.add(new Tuple(dx,dy,p));
-                    }
+        cost[0][0]=grid[0][0];
+        while(!pq.isEmpty()){
+            Tuple t=pq.remove();
+            int curr=t.r,curc=t.c,curd=t.d;
+            for(int i=0;i<4;i++){
+                int dx=x[i]+curr,dy=y[i]+curc;
+                if(dx>=0 && dx<n && dy>=0 && dy<n && curd+grid[dx][dy]<cost[dx][dy]){
+                    cost[dx][dy]=curd+grid[dx][dy];
+                    pq.add(new Tuple(dx,dy,curd+grid[dx][dy]));
                 }
             }
         }
